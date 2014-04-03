9fcc9e598fe2716efd7bdd7d5b4e43e758248fb5
Vik <vik.paruchuri@gmail.com>
2014-04-03 16:22:24 -0400

    Add in about page, update index, fix docular

---
+++ src/app/widgets/realize_about/about.js
@@ -0,0 +1,20 @@
+define(['app', 'angular', 'user'], function (app, angular) {
+	app.register.controller('IndexCtrl', ['$scope', 'user', 'EVENTS', function ($scope, user, EVENTS) {
+
+		$scope.data = {};
+		$scope.switch = function (type) {
+			console.log('switchToLogin.clicked!');
+			$scope.$emit(EVENTS.switchWidgetTree, type, 'default');
+		};
+
+		$scope.register = function () {
+			user.loginOrRegister($scope.data, "register").then(function () {
+
+			}).catch(function (err) {
+				$scope.emailError = err.email;
+				$scope.passwordError = err.password;
+			});
+		};
+
+	}]);
+});
